60f46a95c90cad76f35916d4ab3643f19c6cc73a
Dhia Al-Shalabi <dhia.alshalabi@gmail.com>
2022-06-17 11:12:37 +0300

    Add project files.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,18 @@
+﻿
+using HelloWorld.Math;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var john = new Person();
+            john.FirstName = "Jhon";
+            john.LastName = "Smith";
+            john.Print();
+            Calculator calculator = new Calculator();
+            Console.WriteLine(calculator.Add(1, 2));
+        }
+    }
+}
