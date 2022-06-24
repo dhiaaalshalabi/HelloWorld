1ef0dd27195751232a45eec2cef5f793da1c239d
Dhiaa Shalabi <dhiaa.shalabi@gmail.com>
2022-06-24 18:17:21 +0300

    Add arrays demo.

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,26 +1,40 @@
-﻿
-    using HelloWorld;
-
-    class Program
+﻿class Program
+{
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            var number = 1;
-            Increment(number);
+        var numbers = new[] { 3, 7, 9, 2, 14, 6 };
+        
+        // Length()
+        Console.WriteLine("Length: "+numbers.Length);
+        
+        // IndexOf()
+        var index = Array.IndexOf(numbers, 9);
+        Console.WriteLine("Index of 9: "+index);
+        
+        // Clear()
+        Array.Clear(numbers,0,2);
+        
+        Console.WriteLine("Effect of Clear()");
+        foreach (var number in numbers)
             Console.WriteLine(number);
-            
-            var person = new Person() {Age = 20};
-            MakeOld(person);
-            Console.WriteLine(person.Age);
-        }
-
-        public static void Increment(int number)
-        {
-            number += 10;
-        }
-
-        public static void MakeOld(Person person)
-        {
-            person.Age += 10;
-        }
+        
+        // Copy()
+        int[] another = new int[3];
+        Array.Copy(numbers,another,3);
+        Console.WriteLine("Effect of Copy()");
+        foreach (var i in another)
+            Console.WriteLine(i);
+        
+        // Sort()
+        Array.Sort(numbers);
+        Console.WriteLine("Effect of Sort()");
+        foreach (var i in numbers)
+            Console.WriteLine(i);
+        
+        // Reverse()
+        Array.Reverse(numbers);
+        Console.WriteLine("Effect of Reverse()");
+        foreach (var number in numbers)
+            Console.WriteLine(number);
     }
+}
